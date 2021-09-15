6181d7047a01ddf54472b84442a9588444195c9e
Josh Noel <obsidize@gmail.com>
2021-09-14 21:16:20 -0500

    misc testing updates, add auto-generated docs

--- tests/example-ionic-storage-proxy.spec.ts
+++ tests/example-ionic-storage-proxy.spec.ts
@@ -1,26 +1,70 @@
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+
 import { BrowserStorageTransport, IonicNativeStorageTransport, MockBrowserStorage, MockIonicNativeStorage, ProxyStorageTransport, StorageContext } from '../src';
+
+const createDefaults = () => {
+
+	const mockBrowserTransport = new BrowserStorageTransport(new MockBrowserStorage());
+	const mockNativeTransport = new IonicNativeStorageTransport(new MockIonicNativeStorage(new MockBrowserStorage()));
+
+	const proxyTransport = new ProxyStorageTransport([
+		mockBrowserTransport,
+		mockNativeTransport
+	]);
+
+	const context = new StorageContext(proxyTransport);
+
+	return {
+		mockBrowserTransport,
+		mockNativeTransport,
+		proxyTransport,
+		context
+	};
+};
 
 describe('Example Ionic Native Storage Proxy', () => {
 
-	const createDefaults = () => {
+	it('works with direct references to real storage interfaces', async () => {
 
-		const mockBrowserTransport = new BrowserStorageTransport(new MockBrowserStorage());
-		const mockNativeTransport = new IonicNativeStorageTransport(new MockIonicNativeStorage(new MockBrowserStorage()));
+		const nativeStorage = new NativeStorage();
 
-		const proxyTransport = new ProxyStorageTransport([
-			mockBrowserTransport,
-			mockNativeTransport
-		]);
+		// Create your transports
+		const browserTransport = new BrowserStorageTransport(localStorage);
+		const nativeTransport = new IonicNativeStorageTransport(nativeStorage);
 
+		// Put them in a proxy to allow for hot-swapping between them
+		const proxyTransport = new ProxyStorageTransport([browserTransport, nativeTransport]);
+
+		// Make a context that uses the proxy transport
 		const context = new StorageContext(proxyTransport);
 
-		return {
-			mockBrowserTransport,
-			mockNativeTransport,
-			proxyTransport,
-			context
-		};
-	};
+		expect(proxyTransport.target).toBe(browserTransport);
+		expect(context.transport).toBe(proxyTransport);
+
+		// Swap between transport targets seamlessly without affecting the context
+		proxyTransport.target = nativeTransport;
+		expect(proxyTransport.target).toBe(nativeTransport);
+
+		// The transport will only change if the assignment value is in the transports array passed to the constructor.
+		proxyTransport.target = null;
+		expect(proxyTransport.target).toBe(nativeTransport);
+
+		// Create entities and key-value-pairs with the context
+		// See the StorageContext class for more options
+		const userEmail = context.getKeyValuePair('userEmail');
+
+		spyOn(nativeStorage, 'getItem').and.callFake(() => Promise.resolve('test@test.org'));
+
+		// Load values by one transport
+		const nativeStorageEmail = await userEmail.load();
+		expect(nativeStorageEmail).toBe('test@test.org');
+
+		proxyTransport.target = browserTransport;
+
+		// Or by another, without any changes to the KVP instance or context instance
+		const localStorageEmail = await userEmail.load('DNE'); // you can also pass in a default value
+		expect(localStorageEmail).toBe('DNE');
+	});
 
 	it('allows for seamless hot-swapping between local storage and native storage', async () => {
 
